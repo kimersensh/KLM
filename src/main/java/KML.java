e04f36b7170b4eb32c276e36956af30902fb4f1e
Andrey Dodon <kimersensh@gmail.com>
2016-12-03 22:38:35 +0200

    added getPolygonCoordinates

--- src/main/java/KML.java
+++ src/main/java/KML.java
@@ -22,23 +22,45 @@ public class KML {
 	@Test
 	public void parseKml() {
 		String src = "u2.kml";
-		InputStream is = getClass().getClassLoader().getResourceAsStream(src);
-		Assert.assertNotNull(is);
-		kml = Kml.unmarshal(is);
-		Feature feature = kml.getFeature();
-		parseFeature(feature);
+//		InputStream is = getClass().getClassLoader().getResourceAsStream(src);
+//		Assert.assertNotNull(is);
+//		kml = Kml.unmarshal(is);
+//		Feature feature = kml.getFeature();
+//		parseFeature(feature);
+		getPolygonGeometry(src);
 	}
 
-	private Geometry getGeometry(Kml kml) {
+	private List<Feature> getFeature(Kml kml) {
 		if (kml != null) {
 			Feature feature = kml.getFeature();
 			if (feature instanceof Document) {
 				Document document = (Document) feature;
-				List<Feature> featureList = document.getFeature();
-				for (Feature documentFeature : featureList) {
-					if (documentFeature instanceof Placemark) {
-						Placemark placemark = (Placemark) documentFeature;
-						return placemark.getGeometry();
+				return document.getFeature();
+			}
+		}
+		return null;
+	}
+	
+	
+	public List<Coordinate> getPolygonGeometry(String kmlFile) {
+		Feature feature = Kml.unmarshal(getClass().getClassLoader().getResourceAsStream(kmlFile)).getFeature();
+		if ((feature != null) && (feature instanceof Document)) {
+			List<Feature> featureList = ((Document) feature).getFeature();
+			for (Feature documentFeature : featureList) {
+				if (documentFeature instanceof Placemark) {
+					Geometry geometry = ((Placemark) documentFeature).getGeometry();
+					if ((geometry != null) && (geometry instanceof Polygon)) {
+						Boundary outerBoundaryIs = (Boundary) ((Polygon) geometry).getOuterBoundaryIs();
+						if (outerBoundaryIs != null) {
+							LinearRing linearRing = outerBoundaryIs.getLinearRing();
+							if (linearRing != null) {
+								List<Coordinate> coordinates = linearRing.getCoordinates();
+								if (!coordinates.isEmpty()) {
+									printCoordinates(coordinates);
+									return coordinates;
+								}
+							}
+						} 
 					}
 				}
 			}
@@ -48,8 +70,7 @@ public class KML {
 	
 	
 	private void parseFeature(Feature feature) {
-		if (feature != null) {
-			if (feature instanceof Document) {
+		if ((feature != null)&&(feature instanceof Document)) {
 				Document document = (Document) feature;
 				List<Feature> featureList = document.getFeature();
 				for (Feature documentFeature : featureList) {
@@ -58,7 +79,7 @@ public class KML {
 						Placemark placemark = (Placemark) documentFeature;
 						Geometry geometry = placemark.getGeometry();
 						parseGeometry(geometry);
-					}
+					
 				}
 			}
 		}
@@ -97,8 +118,8 @@ public class KML {
 
 	private void printCoordinates(List<Coordinate> coordinates) {
 		for (Coordinate coordinate : coordinates) {
-			System.out.println("Longitude: " + coordinate.getLongitude());
 			System.out.println("Latitude : " + coordinate.getLatitude());
+			System.out.println("Longitude: " + coordinate.getLongitude()+"\n");
 		}
 	}
 
